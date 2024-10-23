6d12627ee637039c6e0d9773b29bd08ad5bb4bca
Randy G <rjgriggs0072@gmail.com>
2024-10-23 12:11:23 -0700

    Added Misc distro grid upload page

--- pages/Load_Distro_Misc_Items.py
+++ pages/Load_Distro_Misc_Items.py
@@ -115,7 +115,6 @@ def update_options(options):
 # Create a container to hold the file uploader and the dropdown for selecting the chain distro grid.
 # ==================================================================================================================
 
-# Create a container to hold the file uploader and the dropdown for selecting the chain distro grid.
 file_container = st.container()
 with file_container:
     st.subheader(":blue[Distro Grid Fomatting Utility]")
@@ -144,33 +143,6 @@ if selected_option == 'Add new option...':
         st.session_state.new_option = ""
 else:
     st.write(f"You selected: {selected_option}")
-
-    # =====================================================================================================
-    # ADD THE ABILITY TO DOWNLOAD DISTRO_GRID DATA FOR THE SELECTED CHAIN
-    # =====================================================================================================
-
-    # Create a button to download the existing distro_grid data for the selected chain
-    if st.button("Prepare Existing Distro Grid Data download for Selected Chain"):
-        # Fetch existing data for the selected chain from the distro_grid table
-        conn = create_snowflake_connection()[0]
-        query = f"SELECT * FROM distro_grid WHERE chain_name = '{selected_option}'"
-        existing_data = pd.read_sql(query, conn)
-
-        if existing_data.empty:
-            st.warning(f"No existing data found for chain '{selected_option}'.")
-        else:
-            # Convert the existing data to an Excel file and provide a download link
-            download_stream = BytesIO()
-            with pd.ExcelWriter(download_stream, engine='openpyxl') as writer:
-                existing_data.to_excel(writer, sheet_name='Distro Grid Data', index=False)
-            download_stream.seek(0)
-
-            st.download_button(
-                label=f"Download Distro Grid Data for {selected_option}",
-                data=download_stream,
-                file_name=f"{selected_option}_Distro_Grid.xlsx",
-                mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-            )
 
 # ======================================================================================================================
 # USING THE CONTAINER CREATED EARLIER, CREATES THE UPLOADER FOR FORMATTING THE DISTRIBUTION GRID SPREADSHEET.
@@ -231,5 +203,147 @@ if uploaded_file:
                 )
 
 
+# ===========================================================================================================
+# create code uploader in preparation to write to snowflake
+# ==========================================================================================================
 
+# Create a container to hold the file uploader
+snowflake_file_container = st.container()
+
+# Add a title to the container
+with snowflake_file_container:
+    st.markdown("<hr>", unsafe_allow_html=True)
+    st.subheader(":blue[Write Distribution Grid to Snowflake Utility]")
+
+with snowflake_file_container:
+    conn = create_snowflake_connection()[0]  # Get connection object
+
+# Initialize session state variables
+if 'new_option' not in st.session_state:
+    st.session_state.new_option = ""
+if 'option_added' not in st.session_state:
+    st.session_state.option_added = False
+
+# Check if options are available
+if not options:
+    st.warning("No options available. Please add options to the list.")
+else:
+    # Create the dropdown in Streamlit
+    selected_option = st.selectbox(':red[Select the Chain Distro Grid to upload to Snowflake]',
+                                   options + ['Add new option...'], key="existing_chain_option")
+
+    # Check if the selected option is missing and allow the user to add it
+    if selected_option == 'Add new option...':
+        st.write("You selected: Add new option...")
+
+        # Show the form to add a new option
+        with st.form(key='add_option_form', clear_on_submit=True):
+            new_option = st.text_input('Enter the new option', value=st.session_state.new_option)
+            submit_button = st.form_submit_button('Add Option')
+
+            if submit_button and new_option:
+                options.append(new_option)
+                update_options(options)
+                st.success('Option added successfully!')
+                st.session_state.option_added = True
+
+        # Clear the text input field
+        st.session_state.new_option = ""
+
+    else:
+        # Display the selected option
+        st.write(f"You selected: {selected_option}")
+
+# Create file uploader
+uploaded_files = st.file_uploader("Browse or select formatted Distribution Grid excel sheets", type=["xlsx"],
+                                  accept_multiple_files=True)
+
+# Process each uploaded file
+for uploaded_file in uploaded_files:
+    # Read Excel file into pandas ExcelFile object
+    excel_file = pd.ExcelFile(uploaded_file)
+
+    # Get sheet names from ExcelFile object
+    sheet_names = excel_file.sheet_names
+
+    # Process each sheet in the Excel file
+    for sheet_name in sheet_names:
+        df = pd.read_excel(excel_file, sheet_name=sheet_name)
+
+        # Standardize column names to remove leading/trailing whitespace
+        df.columns = df.columns.str.strip()
+
+        # Check if the 'MANUFACTURER' column has any empty cells
+        if 'MANUFACTURER' in df.columns:
+            if df['MANUFACTURER'].isnull().any():
+                st.error(f"The 'MANUFACTURER' column in the uploaded spreadsheet '{uploaded_file.name}', sheet '{sheet_name}', contains empty cells. Please correct these before proceeding.")
+                continue  # Skip further processing for this file
+        else:
+            st.error(f"'MANUFACTURER' column not found in the uploaded file ({uploaded_file.name}). Please make sure the file has the correct format.")
+            continue
+
+        # Validate chain_name in the uploaded file against the selected option to ensure they match
+        if 'CHAIN_NAME' in df.columns:
+            file_chain_name = df['CHAIN_NAME'].iloc[0].strip()  # Ensure there are no leading/trailing spaces
+            if file_chain_name.lower() != selected_option.lower():
+                st.error(f"Chain name in the file ({file_chain_name}) does not match the selected option ({selected_option}). Please correct the file or select the correct chain.")
+                continue  # Skip further processing for this file if chain names do not match
+        else:
+            st.error(f"'CHAIN_NAME' column not found in the uploaded file ({uploaded_file.name}). Please make sure the file has the correct format.")
+            continue
+
+        # Fetch existing data for the selected chain from the distro_grid table
+        query = f"SELECT * FROM distro_grid WHERE chain_name = '{selected_option}'"
+        existing_data = pd.read_sql(query, conn)
+
+        # Check if existing data is not empty
+        if not existing_data.empty:
+            # Compare 'MANUFACTURER' column in both dataframes
+            if 'MANUFACTURER' in existing_data.columns:
+                new_manufacturer_values = set(df['MANUFACTURER'].str.strip().unique())
+                existing_manufacturer_values = set(existing_data['MANUFACTURER'].str.strip().unique())
+
+                # If there is overlap in manufacturer values, warn the user
+                if new_manufacturer_values & existing_manufacturer_values:
+                    st.warning(f"The new data for chain '{selected_option}' contains manufacturers already present in the existing distro_grid table. Please upload the alcohol distro grid spreadsheet first before the misc data.")
+                    continue  # Skip further processing for this file
+
+        # Display DataFrame for each sheet in Streamlit
+        st.write(f"Sheet: {sheet_name} and Chain Selected is: {selected_option}")
+        # st.dataframe(df)
+
+
+        # Write DataFrame to Snowflake on button click
+        button_key = f"import_button_{uploaded_file.name}_{sheet_name}"
+        if st.button("Import Distro Grid into Snowflake", key=button_key):
+            with st.spinner('Uploading data to Snowflake ...'):
+                # Write DataFrame to Snowflake based on the selected store
+                upload_distro_grid_to_snowflake(df, selected_option, update_spinner)
+
+ # ===========================================================================================================
+# Create a button to download the existing distro_grid data for the selected chain
+# ===========================================================================================================
+
+# Create a button to download the existing distro_grid data for the selected chain
+if st.button("Download Existing Distro Grid Data for Selected Chain"):
+    # Fetch existing data for the selected chain from the distro_grid table
+    conn = create_snowflake_connection()[0]
+    query = f"SELECT * FROM distro_grid WHERE chain_name = '{selected_option}'"
+    existing_data = pd.read_sql(query, conn)
+
+    if existing_data.empty:
+        st.warning(f"No existing data found for chain '{selected_option}'.")
+    else:
+        # Convert the existing data to an Excel file and provide a download link
+        download_stream = BytesIO()
+        with pd.ExcelWriter(download_stream, engine='openpyxl') as writer:
+            existing_data.to_excel(writer, sheet_name='Distro Grid Data', index=False)
+        download_stream.seek(0)
+
+        st.download_button(
+            label=f"Download Distro Grid Data for {selected_option}",
+            data=download_stream,
+            file_name=f"{selected_option}_Distro_Grid.xlsx",
+            mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
 
